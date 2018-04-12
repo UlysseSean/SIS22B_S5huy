e357cc150c335a750915fe8fbd7cc7ae5f27b401
UlysseSean <38168564+UlysseSean@users.noreply.github.com>
2018-04-12 14:22:57 -0600

    mi primer version de proyecto

---
+++ clases/crud.js
@@ -0,0 +1,18 @@
+public class CRUD{
+	
+	public function guardar(String campos, String valores, String tabla) {
+		
+	}
+
+	public function actualizar(String campos, String valores, String tabla) {
+	
+	}
+
+	public function consultar(String tabla) {
+		
+	}
+
+	public function eliminar(String id, String tabla) {
+		
+	}
+}
